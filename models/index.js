3ca812761ddb86f49bd263be46c52f45fae62c7f
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-10-23 17:37:20 -0500

    everythings working! still add the routes in insomnia

--- models/index.js
+++ models/index.js
@@ -10,15 +10,18 @@ Product.belongsTo(Category, {
 })
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
+  through: ProductTag,
   foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'product_id'
+  through: ProductTag,
+  foreignKey: 'tag_id'
 })
 module.exports = {
   Product,
